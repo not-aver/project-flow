1acdf38976cc7388e551ea8bf4247ccb26e24411
aver <wyrix0@gmail.com>
2025-12-07 18:22:38 +0300

    DB changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,6 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
-// Always use SQLite in this training project.
-// No environment configuration is needed beyond the storage file path.
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: path.join(__dirname, '../../database.sqlite'),
